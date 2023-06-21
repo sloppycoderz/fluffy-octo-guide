20726f1e1acc43097221227f3c79d7e51cc3b26e
sloppycoderz <bobby.lin+gg@crypto.com>
2023-06-21 18:23:38 +0800

    Edit file_2

--- file_2.py
+++ file_2.py
@@ -1,3 +1,3 @@
 import requests
 
-headers = {"authorization": "Basic dGVzdEBleGFtcGxlLmNvbTphbGRqb2oyMWoycDFqM3AxajM="}
+headers = {"authorization": "Basic Ym9iYnlsZWU6eUF6ZzZfUE5jcFE4X1JjclBvQ0E="}
